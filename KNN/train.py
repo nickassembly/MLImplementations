86c64723c89490e9b96a02050279c9da98dea6d6
nickguerra <nickg@maliciousgames.com>
2023-02-03 05:26:48 -0600

    Added linear regression implementation

--- KNN/train.py
+++ KNN/train.py
@@ -1,9 +1,9 @@
+from KNN import KNN
 import numpy as np
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
-from KNN import KNN
 
 cmap = ListedColormap(['#FF0000','#00FF00','#0000FF'])
 
